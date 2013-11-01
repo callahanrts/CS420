efeec47be03949888c4aa62799399e2f87714ee8
Cody Callahan <CallahanRTS@gmail.com>
2013-10-31 17:37:14 -0700

    Initial Commit

---
+++ CS420/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CS420
+//
+//  Created by Cody Callahan on 10/31/13.
+//  Copyright (c) 2013 Cody Callahan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
